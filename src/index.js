f4d9d7e9af7dd3b56a024c215d07c860ffdf2a21
Carlos Castellon <carlos.castellon.91@gmail.com>
2021-08-09 22:18:04 -0700

    third commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 // Import the React and ReactDOM libraries
+import { blue, white } from 'chalk';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a React Component
 const App = () => {
-    return  <div>Hi there!</div>;
+
+    return  (
+    <div>
+        <label class="label" for="name">
+            Enter name:
+        </label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {getButtonText()}
+        </button> 
+    </div>
+        )
 }
 
 // Take the React component and show it on the screen
